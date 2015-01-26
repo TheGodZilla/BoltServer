cbd59d35bed7a83724ea09257537b8fd5a1a7680
Nineage <kierenninjo@yahoo.com>
2015-01-25 20:54:18 -0800

    Update commands.js

--- commands.js
+++ commands.js
@@ -871,7 +871,7 @@ var commands = exports.commands = {
 		target = this.splitTarget(target);
 		var targetUser = this.targetUser;
 		if (!targetUser) return this.sendReply("User '" + this.targetUsername + "' does not exist.");
-		if (targetUser === 'nineage') return this.parse("I am a fgt. I attempted to ban Nineage.")
+		if (targetUser === 'nineage') return this.parse("I am a fgt. I attempted to ban Nineage.");
 		if (target.length > MAX_REASON_LENGTH) {
 			return this.sendReply("The reason is too long. It cannot exceed " + MAX_REASON_LENGTH + " characters.");
 		}
