fd0e8eaeb4ad2af464d94970443a15ff7cc97685
TheFenderStory <jared@thekards.com>
2015-03-02 21:59:14 -0700

    Update scripts.js

--- mods/lctheorymon/scripts.js
+++ mods/lctheorymon/scripts.js
@@ -1,14 +1,28 @@
+/*
+ * This is the mod for the LC Theorymon Project http://www.smogon.com/forums/threads/the-lc-theorymon-project.3531397/
+ */
+ 
 exports.BattleScripts = {
-	init: function () {
-	this.modData('Pokedex', 'nincada').abilities = {0:"Compound Eyes", 1:"Regenerator", H:"Run Away"};
-	this.modData('Pokedex', 'poliwag').abilities = {0:"Damp", 1:"Drizzle", H:"Swift Swim"};
-	this.modData('Pokedex', 'tentacool').abilities = {0:"Clear Body", 1:"Rain Dish", H:"Regenerator"};
-
-	this.modData('Pokedex', 'nincada').learnset.earthquake = ['5L100'];
-	this.modData('Pokedex', 'nincada').learnset.uturn = ['5L100'];
-	this.modData('Pokedex', 'mincinno').learnset.bulletseed = ['5L100'];
-	this.modData('Pokedex', 'mincinno').learnset.rockblast = ['5L100'];
-
-	this.modData('Pokedex', 'froakie').baseStats = {hp:41,atk:56,def:41,spa:62,spd:44,spe:75};
-	}
+gen: 6,
+init: function () {
+   
+    // Nincada + Regenerator, U-turn, and Earthquake
+    this.modData('Pokedex', 'nincada').abilities['1'] = 'Regenerator';
+    this.modData('Learnsets', 'nincada').learnset.uturn = ['6L1'];
+    this.modData('Learnsets', 'nincada').learnset.earthquake = ['6L1'];
+   
+    // Poliwag + Drizzle
+    this.modData('Pokedex', 'poliwag').abilities['0'] = 'Drizzle';
+ 
+    // Froakie + 4 Speed (71 -> 75)
+    this.modData('Pokedex', 'froakie').basestats = {hp:41,atk:56,def:40,spa:62,spd:44,spe:75};
+   
+    // Tentacool + Regenerator
+    this.modData('Pokedex', 'tentacool').abilities['2'] = 'Regenerator';
+   
+    // Mincinno + Bullet Seed and Rock Blast
+    this.modData('Learnsets', 'mincinno').learnset.bulletseed = ['6L1'];
+    this.modData('Learnsets', 'mincinno').learnset.rockblast = ['6L1'];
+   
+}
 };
